ccdca48fefa48a58c29fe8c8b2aa5a8f5fa29e41
bismowibi <bismowibi@gmail.com>
2023-09-19 23:19:02 +0700

    style : setup font & tailwind config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,22 @@
 import "@/styles/globals.css";
 
+import { Inter } from "next/font/google";
+
 import { NextUIProvider } from "@nextui-org/react";
+
+const inter = Inter({
+  weight: ["400", "500", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
     </NextUIProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "primary-brand": {
+          50: "#FFEFD7",
+          100: "#FFE0B0",
+          200: "#FFBE75",
+          300: "#FF9543",
+          400: "#F36A1D",
+          500: "#BD5724",
+          600: "#8B4527",
+          700: "#5F3424",
+        },
+        "neutral-gray": {
+          50: "#F9F8F7",
+          100: "#F4F3F1",
+          200: "#E2E1DF",
+          300: "#C3C3BF",
+          400: "#B4B2AF",
+          500: "#777574",
+          600: "#8B4527",
+          700: "#353432",
+          800: "#232221",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
